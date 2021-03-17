e547de2859ae6fe02a05b2d683268ca66a07f567
rajutmg <rajutmg619@gmail.com>
2021-03-17 17:13:43 +0545

    api

--- src/components/part3/api/MainAPI.jsx
+++ src/components/part3/api/MainAPI.jsx
@@ -3,15 +3,33 @@ import axios from 'axios';
 
 const MainAPI = () => {
     const [num, setnum] = useState();
+    const [name, setname] = useState();
+    const [moves, setmoves] = useState();
+    useEffect(() => {
+        async function getData(){
+            const res = await axios.get(`https://pokeapi.co/api/v2/pokemon/${num}`);
+            console.log(res);
+            // console.log(res.data.name);
+            setname(res.data.name);
+            setmoves(res.data.moves.length);
+        }
+        getData();
+    },)
     return(
         <>
-        <h1>You choose  {num}  value</h1>
+        <h1>You choose  {num}  value <br/> My name is {name} <br/> I have {moves} moves</h1>
         <select value={num} onChange={(event) =>{
            setnum(event.target.value)
         }} >
             <option value="1">1</option>
             <option value="2">2</option>
-            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+            <option value="6">6</option>
+            <option value="7">7</option>
+            <option value="8">8</option>
+            <option value="9">9</option>
+            <option value="10">10</option>
         </select>
         </>
     );
